c85815791e250259f20a53da04af2523b29785a4
ruslanchampion <ruslan.timbakov@gmail.com>
2020-10-21 17:38:29 +0800

    feat: wather v 1.0

--- momentum/app/weather.js
+++ momentum/app/weather.js
@@ -5,29 +5,36 @@ const forecast = document.querySelector('.forecast');
 const humidity = document.querySelector('.humidity');
 const speed = document.querySelector('.speed');
 const pressure = document.querySelector('.pressure');
+const error = document.querySelector('.error'); 
 
 
 async function getWeather() {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value || 'Minsk'}&appid=4468abd9423421e94dcc96dbe813f5ac&units=metric`;
-  const res = await fetch(url);
-  
-  const data = await res.json();
-  
+  const response  = await fetch(url);
+  const data = await response.json()
+
+  if (response.ok) {
+    error.textContent = ''
+  } else {
+    error.textContent = '!!!WORNG CITY!!!'
+    localStorage.setItem('city', 'Minsk');
+    city.placeholder = localStorage.getItem('city')
+  }
+
+
   weatherIcon.className = 'weather-icon owf';
   weatherIcon.classList.add(`owf-${data.weather[0].id}`);
   temperature.textContent = `Temperature: ${data.main.temp.toFixed(0)}°C`;
   forecast.textContent = `${data.weather[0].description.toUpperCase()}`;
   humidity.textContent = `Humidity: ${data.main.humidity}%`
   speed.textContent = `Wind Speed: ${data.wind.speed} m/s `
-  pressure.textContent = `Pressure: ${data.main.pressure} ISA`
+  pressure.textContent = `Pressure: ${data.main.pressure} ISA`                       
  
 }
-getWeather().catch(alert)
-getWeather(404)
-  .catch(e => console.log(`Error! ${e}`))
+
 function getCity() {
   if (localStorage.getItem('city') === null) {
-    city.placeholder = '[Press Your City]';
+    city.placeholder = '[Your City]';
   } else {
     city.placeholder = localStorage.getItem('city')
       city.value = localStorage.getItem('city');
@@ -44,7 +51,7 @@ function setCity(e) {
         city.blur();
       }
       else if  (city.value == '' && localStorage.getItem('city') === null) {
-        city.placeholder = '[Press Your City]';
+        city.placeholder = '[Your City]';
         city.blur();
          }
       else {
@@ -60,7 +67,7 @@ function setCity(e) {
         getWeather();
       } 
      else if  (city.value == '' && localStorage.getItem('city') === null) {
-      city.placeholder = '[Press Your City]';
+      city.placeholder = '[Your City]';
       }
       else {
           localStorage.setItem('city', city.value);
