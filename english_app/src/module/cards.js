d1d4ed5ec3757604176e7ec021543a27dc47f8c9
ruslanchampion <ruslan.timbakov@gmail.com>
2020-12-08 14:38:56 +0800

    update: add all kinda assets

---
+++ english_app/src/module/cards.js
@@ -0,0 +1,549 @@
+/* eslint-disable import/no-unresolved */
+import cry from '../assets/images/cry.jpg';
+import dance from '../assets/images/dance.jpg';
+import dive from '../assets/images/dive.jpg';
+import draw from '../assets/images/draw.jpg';
+import fishing from '../assets/images/fishing.jpg';
+import fly from '../assets/images/fly.jpg';
+import hug from '../assets/images/hug.jpg';
+import jump from '../assets/images/jump.jpg';
+
+import cryAudio from '../assets/audio/cry.mp3';
+import danceAudio from '../assets/audio/dance.mp3';
+import diveAudio from '../assets/audio/dive.mp3';
+import drawAudio from '../assets/audio/draw.mp3';
+import fishingAudio from '../assets/audio/fishing.mp3';
+import flyAudio from '../assets/audio/fly.mp3';
+import hugAudio from '../assets/audio/hug.mp3';
+import jumpAudio from '../assets/audio/jump.mp3';
+
+import open from '../assets/images/open.jpg';
+import play from '../assets/images/play.jpg';
+import point from '../assets/images/point.jpg';
+import ride from '../assets/images/ride.jpg';
+import run from '../assets/images/run.jpg';
+import sing from '../assets/images/sing.jpg';
+import skip from '../assets/images/skip.jpg';
+import swim from '../assets/images/swim.jpg';
+
+import openAudio from '../assets/audio/open.mp3';
+import playAudio from '../assets/audio/play.mp3';
+import pointAudio from '../assets/audio/point.mp3';
+import rideAudio from '../assets/audio/ride.mp3';
+import runAudio from '../assets/audio/run.mp3';
+import singAudio from '../assets/audio/sing.mp3';
+import skipAudio from '../assets/audio/skip.mp3';
+import swimAudio from '../assets/audio/swim.mp3';
+
+import argue from '../assets/images/argue.jpg';
+import build from '../assets/images/build.jpg';
+import carry from '../assets/images/carry.jpg';
+import catCh from '../assets/images/catch.jpg';
+import drive from '../assets/images/drive.jpg';
+import drop from '../assets/images/drop.jpg';
+import pull from '../assets/images/pull.jpg';
+import push from '../assets/images/push.jpg';
+
+import argueAudio from '../assets/audio/argue.mp3';
+import buildAudio from '../assets/audio/build.mp3';
+import carryAudio from '../assets/audio/carry.mp3';
+import catchAudio from '../assets/audio/catch.mp3';
+import driveAudio from '../assets/audio/drive.mp3';
+import dropAudio from '../assets/audio/drop.mp3';
+import pullAudio from '../assets/audio/pull.mp3';
+import pushAudio from '../assets/audio/push.mp3';
+
+import big from '../assets/images/big.jpg';
+import small from '../assets/images/small.jpg';
+import fast from '../assets/images/fast.jpg';
+import slow from '../assets/images/slow.jpg';
+import friendly from '../assets/images/friendly.jpg';
+import unfriendly from '../assets/images/unfriendly.jpg';
+import old from '../assets/images/old.jpg';
+import young from '../assets/images/young.jpg';
+
+import bigAudio from '../assets/audio/big.mp3';
+import smallAudio from '../assets/audio/small.mp3';
+import fastAudio from '../assets/audio/fast.mp3';
+import slowAudio from '../assets/audio/slow.mp3';
+import friendlyAudio from '../assets/audio/friendly.mp3';
+import unfriendlyAudio from '../assets/audio/unfriendly.mp3';
+import oldAudio from '../assets/audio/old.mp3';
+import youngAudio from '../assets/audio/young.mp3';
+
+import cat from '../assets/images/cat.jpg';
+import chick from '../assets/images/chick.jpg';
+import chicken from '../assets/images/chicken.jpg';
+import dog from '../assets/images/dog.jpg';
+import horse from '../assets/images/horse.jpg';
+import pig from '../assets/images/pig.jpg';
+import rabbit from '../assets/images/rabbit.jpg';
+import sheep from '../assets/images/sheep.jpg';
+
+import catAudio from '../assets/audio/cat.mp3';
+import chickAudio from '../assets/audio/chick.mp3';
+import chickenAudio from '../assets/audio/chicken.mp3';
+import dogAudio from '../assets/audio/dog.mp3';
+import horseAudio from '../assets/audio/horse.mp3';
+import pigAudio from '../assets/audio/pig.mp3';
+import rabbitAudio from '../assets/audio/rabbit.mp3';
+import sheepAudio from '../assets/audio/sheep.mp3';
+
+import bird from '../assets/images/bird.jpg';
+import fish from '../assets/images/fish.jpg';
+import frog from '../assets/images/frog.jpg';
+import giraffe from '../assets/images/giraffe.jpg';
+import lion from '../assets/images/lion.jpg';
+import mouse from '../assets/images/mouse.jpg';
+import turtle from '../assets/images/turtle.jpg';
+import dolphin from '../assets/images/dolphin.jpg';
+
+import birdAudio from '../assets/audio/bird.mp3';
+import fishAudio from '../assets/audio/fish.mp3';
+import frogAudio from '../assets/audio/frog.mp3';
+import giraffeAudio from '../assets/audio/giraffe.mp3';
+import lionAudio from '../assets/audio/lion.mp3';
+import mouseAudio from '../assets/audio/mouse.mp3';
+import turtleAudio from '../assets/audio/turtle.mp3';
+import dolphinAudio from '../assets/audio/dolphin.mp3';
+
+import skirt from '../assets/images/skirt.jpg';
+import pants from '../assets/images/pants.jpg';
+import blouse from '../assets/images/blouse.jpg';
+import dress from '../assets/images/dress.jpg';
+import boot from '../assets/images/boot.jpg';
+import shirt from '../assets/images/shirt.jpg';
+import coat from '../assets/images/coat.jpg';
+import shoe from '../assets/images/shoe.jpg';
+
+import skirtAudio from '../assets/audio/skirt.mp3';
+import pantsAudio from '../assets/audio/pants.mp3';
+import blouseAudio from '../assets/audio/blouse.mp3';
+import dressAudio from '../assets/audio/dress.mp3';
+import bootAudio from '../assets/audio/boot.mp3';
+import shirtAudio from '../assets/audio/shirt.mp3';
+import coatAudio from '../assets/audio/coat.mp3';
+import shoeAudio from '../assets/audio/shoe.mp3';
+
+import sad from '../assets/images/sad.jpg';
+import angry from '../assets/images/angry.jpg';
+import happy from '../assets/images/happy.jpg';
+import tired from '../assets/images/tired.jpg';
+import surprised from '../assets/images/surprised.jpg';
+import scared from '../assets/images/scared.jpg';
+import smile from '../assets/images/smile.jpg';
+import laugh from '../assets/images/laugh.jpg';
+
+import sadAudio from '../assets/audio/sad.mp3';
+import angryAudio from '../assets/audio/angry.mp3';
+import happyAudio from '../assets/audio/happy.mp3';
+import tiredAudio from '../assets/audio/tired.mp3';
+import surprisedAudio from '../assets/audio/surprised.mp3';
+import scaredAudio from '../assets/audio/scared.mp3';
+import smileAudio from '../assets/audio/smile.mp3';
+import laughAudio from '../assets/audio/laugh.mp3';
+
+const actionSetA = [
+  {
+    word: 'cry',
+    translation: 'плакать',
+    image: cry,
+    audioSrc: cryAudio,
+  },
+  {
+    word: 'dance',
+    translation: 'танцевать',
+    image: dance,
+    audioSrc: danceAudio,
+  },
+  {
+    word: 'dive',
+    translation: 'нырять',
+    image: dive,
+    audioSrc: diveAudio,
+  },
+  {
+    word: 'draw',
+    translation: 'рисовать',
+    image: draw,
+    audioSrc: drawAudio,
+  },
+  {
+    word: 'fishing',
+    translation: 'ловить рыбу',
+    image: fishing,
+    audioSrc: fishingAudio,
+  },
+  {
+    word: 'fly',
+    translation: 'летать',
+    image: fly,
+    audioSrc: flyAudio,
+  },
+  {
+    word: 'hug',
+    translation: 'обнимать',
+    image: hug,
+    audioSrc: hugAudio,
+  },
+  {
+    word: 'jump',
+    translation: 'прыгать',
+    image: jump,
+    audioSrc: jumpAudio,
+  },
+];
+const actionSetB = [
+  {
+    word: 'open',
+    translation: 'открывать',
+    image: open,
+    audioSrc: openAudio,
+  },
+  {
+    word: 'play',
+    translation: 'играть',
+    image: play,
+    audioSrc: playAudio,
+  },
+  {
+    word: 'point',
+    translation: 'указывать',
+    image: point,
+    audioSrc: pointAudio,
+  },
+  {
+    word: 'ride',
+    translation: 'ездить',
+    image: ride,
+    audioSrc: rideAudio,
+  },
+  {
+    word: 'run',
+    translation: 'бегать',
+    image: run,
+    audioSrc: runAudio,
+  },
+  {
+    word: 'sing',
+    translation: 'петь',
+    image: sing,
+    audioSrc: singAudio,
+  },
+  {
+    word: 'skip',
+    translation: 'пропускать, прыгать',
+    image: skip,
+    audioSrc: skipAudio,
+  },
+  {
+    word: 'swim',
+    translation: 'плавать',
+    image: swim,
+    audioSrc: swimAudio,
+  },
+];
+const actionSetC = [
+  {
+    word: 'argue',
+    translation: 'спорить',
+    image: argue,
+    audioSrc: argueAudio,
+  },
+  {
+    word: 'build',
+    translation: 'строить',
+    image: build,
+    audioSrc: buildAudio,
+  },
+  {
+    word: 'carry',
+    translation: 'нести',
+    image: carry,
+    audioSrc: carryAudio,
+  },
+  {
+    word: 'catch',
+    translation: 'ловить',
+    image: catCh,
+    audioSrc: catchAudio,
+  },
+  {
+    word: 'drive',
+    translation: 'водить машину',
+    image: drive,
+    audioSrc: driveAudio,
+  },
+  {
+    word: 'drop',
+    translation: 'падать',
+    image: drop,
+    audioSrc: dropAudio,
+  },
+  {
+    word: 'pull',
+    translation: 'тянуть',
+    image: pull,
+    audioSrc: pullAudio,
+  },
+  {
+    word: 'push',
+    translation: 'толкать',
+    image: push,
+    audioSrc: pushAudio,
+  },
+];
+const adjective = [
+  {
+    word: 'big',
+    translation: 'большой',
+    image: big,
+    audioSrc: bigAudio,
+  },
+  {
+    word: 'small',
+    translation: 'маленький',
+    image: small,
+    audioSrc: smallAudio,
+  },
+  {
+    word: 'fast',
+    translation: 'быстрый',
+    image: fast,
+    audioSrc: fastAudio,
+  },
+  {
+    word: 'slow',
+    translation: 'медленный',
+    image: slow,
+    audioSrc: slowAudio,
+  },
+  {
+    word: 'friendly',
+    translation: 'дружелюбный',
+    image: friendly,
+    audioSrc: friendlyAudio,
+  },
+  {
+    word: 'unfriendly',
+    translation: 'недружелюбный',
+    image: unfriendly,
+    audioSrc: unfriendlyAudio,
+  },
+  {
+    word: 'old',
+    translation: 'старый',
+    image: old,
+    audioSrc: oldAudio,
+  },
+  {
+    word: 'young',
+    translation: 'молодой',
+    image: young,
+    audioSrc: youngAudio,
+  },
+];
+const animalSetA = [
+  {
+    word: 'cat',
+    translation: 'кот',
+    image: cat,
+    audioSrc: catAudio,
+  },
+  {
+    word: 'chick',
+    translation: 'цыплёнок',
+    image: chick,
+    audioSrc: chickAudio,
+  },
+  {
+    word: 'chicken',
+    translation: 'курица',
+    image: chicken,
+    audioSrc: chickenAudio,
+  },
+  {
+    word: 'dog',
+    translation: 'собака',
+    image: dog,
+    audioSrc: dogAudio,
+  },
+  {
+    word: 'horse',
+    translation: 'лошадь',
+    image: horse,
+    audioSrc: horseAudio,
+  },
+  {
+    word: 'pig',
+    translation: 'свинья',
+    image: pig,
+    audioSrc: pigAudio,
+  },
+  {
+    word: 'rabbit',
+    translation: 'кролик',
+    image: rabbit,
+    audioSrc: rabbitAudio,
+  },
+  {
+    word: 'sheep',
+    translation: 'овца',
+    image: sheep,
+    audioSrc: sheepAudio,
+  },
+];
+const animalSetB = [
+  {
+    word: 'bird',
+    translation: 'птица',
+    image: bird,
+    audioSrc: birdAudio,
+  },
+  {
+    word: 'fish',
+    translation: 'рыба',
+    image: fish,
+    audioSrc: fishAudio,
+  },
+  {
+    word: 'frog',
+    translation: 'жаба',
+    image: frog,
+    audioSrc: frogAudio,
+  },
+  {
+    word: 'giraffe',
+    translation: 'жираф',
+    image: giraffe,
+    audioSrc: giraffeAudio,
+  },
+  {
+    word: 'lion',
+    translation: 'лев',
+    image: lion,
+    audioSrc: lionAudio,
+  },
+  {
+    word: 'mouse',
+    translation: 'мышь',
+    image: mouse,
+    audioSrc: mouseAudio,
+  },
+  {
+    word: 'turtle',
+    translation: 'черепаха',
+    image: turtle,
+    audioSrc: turtleAudio,
+  },
+  {
+    word: 'dolphin',
+    translation: 'дельфин',
+    image: dolphin,
+    audioSrc: dolphinAudio,
+  },
+];
+const clothes = [
+  {
+    word: 'skirt',
+    translation: 'юбка',
+    image: skirt,
+    audioSrc: skirtAudio,
+  },
+  {
+    word: 'pants',
+    translation: 'брюки',
+    image: pants,
+    audioSrc: pantsAudio,
+  },
+  {
+    word: 'blouse',
+    translation: 'блузка',
+    image: blouse,
+    audioSrc: blouseAudio,
+  },
+  {
+    word: 'dress',
+    translation: 'платье',
+    image: dress,
+    audioSrc: dressAudio,
+  },
+  {
+    word: 'boot',
+    translation: 'ботинок',
+    image: boot,
+    audioSrc: bootAudio,
+  },
+  {
+    word: 'shirt',
+    translation: 'рубашка',
+    image: shirt,
+    audioSrc: shirtAudio,
+  },
+  {
+    word: 'coat',
+    translation: 'пальто',
+    image: coat,
+    audioSrc: coatAudio,
+  },
+  {
+    word: 'shoe',
+    translation: 'туфли',
+    image: shoe,
+    audioSrc: shoeAudio,
+  },
+];
+const emotion = [
+  {
+    word: 'sad',
+    translation: 'грустный',
+    image: sad,
+    audioSrc: sadAudio,
+  },
+  {
+    word: 'angry',
+    translation: 'сердитый',
+    image: angry,
+    audioSrc: angryAudio,
+  },
+  {
+    word: 'happy',
+    translation: 'счастливый',
+    image: happy,
+    audioSrc: happyAudio,
+  },
+  {
+    word: 'tired',
+    translation: 'уставший',
+    image: tired,
+    audioSrc: tiredAudio,
+  },
+  {
+    word: 'surprised',
+    translation: 'удивлённый',
+    image: surprised,
+    audioSrc: surprisedAudio,
+  },
+  {
+    word: 'scared',
+    translation: 'испуганный',
+    image: scared,
+    audioSrc: scaredAudio,
+  },
+  {
+    word: 'smile',
+    translation: 'улыбка',
+    image: smile,
+    audioSrc: smileAudio,
+  },
+  {
+    word: 'laugh',
+    translation: 'смех',
+    image: laugh,
+    audioSrc: laughAudio,
+  },
+];
+
+export default [
+  actionSetA, actionSetB, actionSetC, adjective, animalSetA, animalSetB, clothes, emotion,
+];
